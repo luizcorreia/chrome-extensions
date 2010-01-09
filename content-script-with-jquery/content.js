445bc081c4e1869f3c8d8b059656dce65f504725
remi <remi@remitaylor.com>
2010-01-08 22:08:51 -0700

    added a content script that uses jquery

---
+++ content-script-with-jquery/content.js
@@ -0,0 +1,19 @@
+console.log("Loaded content script with jQuery for " + window.location.href);
+
+// for some reason, let's hide the commit, path, browser, and readme name ... so the first 
+// thing we see is the readme text
+$(document).ready(function(){
+
+  var elements = $("#commit, #path, #browser, #readme .name").hide();
+  var toggle_link = $('<a href="#">toggle details</a>').css({
+    textAlign: 'right',
+    width:     '100%',
+    display:   'inline-block'
+  });
+  toggle_link.click(function(){
+    elements.toggle();
+  });
+
+  $("#commit").before(toggle_link);
+
+});

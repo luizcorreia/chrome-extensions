925732bee72e2d05f1714e149271cc41ef6594b0
remi <remi@remitaylor.com>
2010-01-08 22:12:40 -0700

    DOM ready won't always fire because sometimes it's ALREADY loaded when our script runs so we can get rid of it

--- content-script-with-jquery/content.js
+++ content-script-with-jquery/content.js
@@ -2,18 +2,18 @@ console.log("Loaded content script with jQuery for " + window.location.href);
 
 // for some reason, let's hide the commit, path, browser, and readme name ... so the first 
 // thing we see is the readme text
-$(document).ready(function(){
+console.log("DOM ready");
 
-  var elements = $("#commit, #path, #browser, #readme .name").hide();
-  var toggle_link = $('<a href="#">toggle details</a>').css({
-    textAlign: 'right',
-    width:     '100%',
-    display:   'inline-block'
-  });
-  toggle_link.click(function(){
-    elements.toggle();
-  });
-
-  $("#commit").before(toggle_link);
-
+var elements = $("#commit, #path, #browser, #readme .name").hide();
+var toggle_link = $('<a href="#">toggle details</a>').css({
+  textAlign: 'right',
+  width:     '100%',
+  display:   'inline-block'
 });
+toggle_link.click(function(){
+  elements.toggle();
+});
+
+$("#commit").before(toggle_link);
+
+console.log("done");
